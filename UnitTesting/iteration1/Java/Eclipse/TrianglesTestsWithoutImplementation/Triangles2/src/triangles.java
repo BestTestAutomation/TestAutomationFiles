a880ecd4b297a9210c95d05e26832db91f35f1c1
Dan Geiger <dan.geiger@bellevuecollege.edu>
2022-07-17 09:27:47 -0700

    supplementary code

---
+++ UnitTesting/iteration1/Java/Eclipse/TrianglesTestsWithoutImplementation/Triangles2/src/triangles.java
@@ -0,0 +1,14 @@
+
+public class triangles {
+	public boolean IsTriangle(double side1, double side2, double side3)
+    {
+		// Put your implementation here
+        return false;
+    }
+	
+	public String OutputTriangleType(double side1, double side2, double side3)
+    {        
+		// Put your implementation here
+        return null;
+    }
+}
